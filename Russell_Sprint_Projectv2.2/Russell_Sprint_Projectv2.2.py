a17e9343e99065fcf50d79788c2b646b80b86f3c
Russellouo <rchien25_student@ivycollegiate.org>
2024-10-16 06:15:35 +0000

    end class

---
+++ Russell_Sprint_Projectv2.2/Russell_Sprint_Projectv2.2.py
@@ -0,0 +1,8 @@
+my_numbers = [5, 10, 15, 20, 25]
+print(my_numbers[2])
+my_strings = ["apple", "banana", "cherry"]
+print(my_strings[-1])
+mixed_list = [1, "two", 3.0, [4, 5]]
+print(mixed_list[1])
+my_numbers.append(30)
+print(my_numbers)
