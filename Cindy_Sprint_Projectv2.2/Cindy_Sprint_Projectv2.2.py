9ccef0e53a7569c131ac98a8126e77e952f376a2
Cindy0121 <cchen25_student@ivycollegiate.org>
2024-10-16 06:06:22 +0000

    Creates three lists

---
+++ Cindy_Sprint_Projectv2.2/Cindy_Sprint_Projectv2.2.py
@@ -0,0 +1,8 @@
+my_numbers = [5, 10, 15, 20, 25]
+print(my_numbers[2])
+
+my_strings = ["apple", "banana", "cherry"]
+print (my_strings[-1])
+
+mixed_list = [1, "two", 3.0, [4, 0]]
+print (mixed_list[1])
